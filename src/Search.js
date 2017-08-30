eea9af3a4193c8a01165f1eb5ac6ccb1a975d9ca
dr-binaryV1 <developerdamian@gmail.com>
2017-08-30 13:42:51 -0500

    feat: Implement Search by Author and Title

--- src/Search.js
+++ src/Search.js
@@ -1,52 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp';
+
+import Book from './Book';
 import * as BooksAPI from './BooksAPI'
 
-const Search = (props) => {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <a className="close-search" onClick={() => props.onBookShelfNavigate()}>Close</a>
-        <div className="search-books-input-wrapper">
-          {/* 
-            NOTES: The search from BooksAPI is limited to a particular set of search terms.
-            You can find these search terms here:
-            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+class Search extends Component {
+  state = {
+    query: ''
+  }
+
+  onInputChange = (e) => {
+    this.setState({ query: e.target.value });
+  }
+
+  render() {
+    let showingBooks;
+    const { books, onBookShelfNavigate } = this.props;
+    const { query } = this.state;
+
+    if(query) {
+      const match = new RegExp(escapeRegExp(query), 'i');
+      showingBooks = books.filter((book) => match.test(book.title) || match.test(book.authors));
+    } else {
+        showingBooks = books;
+    }
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a className="close-search" onClick={() => onBookShelfNavigate()}>Close</a>
+          <div className="search-books-input-wrapper">
+            {/* 
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+              
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input 
+              onChange={this.onInputChange} 
+              type="text" 
+              value={this.state.query}
+              placeholder="Search by title or author"/>
             
-            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-            you don't find a specific author or title. Every search is limited by search terms.
-          */}
-          <input type="text" placeholder="Search by title or author"/>
-          
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {showingBooks.map((book) => { return <Book key={book.id} book={book} />  })}
+          </ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid">
-          {props.books.map((book) => {
-            return (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
-              </li>
-            )
-          })}
-        </ol>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Search;
