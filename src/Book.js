4d4b627c64845f540d11bc4164a36b247eb79e16
dr-binaryV1 <developerdamian@gmail.com>
2017-08-31 00:43:16 -0500

    fix: Change default value setter and add ternary operator to set the 'none' attr

--- src/Book.js
+++ src/Book.js
@@ -13,7 +13,7 @@ function Book(props) {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
-            <select defaultValue={book.shelf} onChange={optionChanged}>
+            <select defaultValue={book.shelf ? book.shelf : 'none'} onChange={optionChanged}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
